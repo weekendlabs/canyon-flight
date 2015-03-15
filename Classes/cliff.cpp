9ecf9b8daf25132a141d6f6b43ab52567fd086ea
Santhos Baala RS <santhosbaala@systeminsights.com>
2015-03-15 16:37:32 +0530

    :gem: Cliffs -> black and white

--- Classes/cliff.cpp
+++ Classes/cliff.cpp
@@ -11,14 +11,26 @@ Cliff::Cliff() {
 }
 
 void Cliff::spawnCliff(cocos2d::Layer * layer) {
+//    double counterInc = cocos2d::random(SCALE_640_WIDTH(0.075, visibleSize.width), SCALE_640_WIDTH(0.15, visibleSize.width));
     counter += SCALE_960_HEIGHT(0.075, visibleSize.height);
     double sineValue = sin(counter);
-    double gapWidth = SCALE_640_WIDTH(visibleSize.width * 0.5, visibleSize.width);
+    counter += sin(counter * 0.01);
+    
+    // |-- targetX -|-gw--|- remaining -| = visibleWidth
+    // |-Left Cliff-|-Gap-|-Right Cliff-| = visibleWidth
+    
+    // visibleWidth * 0.25 <= gapWidth <= visibleWidth * 0.5
+    const double MAX_GAP_WIDTH = SCALE_640_WIDTH(visibleSize.width * 0.5, visibleSize.width);
+    const double MIN_GAP_WIDTH = SCALE_640_WIDTH(visibleSize.width * 0.25, visibleSize.width);
+//    double gapWidth = ((sineValue - -1.0) / (1.0 - -1.0)) * (MAX_GAP_WIDTH - MIN_GAP_WIDTH) + MIN_GAP_WIDTH;
+    double gapWidth = MAX_GAP_WIDTH;
     
     // ((Input - InputLow) / (InputHigh - InputLow)) * (OutputHigh - OutputLow) + OutputLow;
     // Sine value ranges from -1 to +1 and width ranges from  gapWidth to visibleSize.width - gapWidth
+    // -1.0 <= x <= +1.0 ---> gapWidth <= targetX <= (visibleWidth - gapWidth)
     double targetX = ((sineValue - -1.0) / (1.0 - -1.0)) * (visibleSize.width - gapWidth) + gapWidth;
     
+    CCLOG("Counter value: %.2f", counter);
     CCLOG("Sine Value: %.2f", sineValue);
     CCLOG("Target X  : %.2f", targetX);
     
@@ -30,42 +42,24 @@ void Cliff::spawnCliff(cocos2d::Layer * layer) {
     Sprite* leftCliff = Sprite::create("cliff.png");
     
     leftCliff->setScaleX(leftCliffWidth / leftCliff->getContentSize().width);
+    leftCliff->setColor(Color3B::BLACK);
     leftCliff->setPosition(Point(origin.x + leftCliffWidth * 0.5, visibleSize.height + leftCliff->getContentSize().height));
 
     layer->addChild(leftCliff, 1);
-    
-    // ------- Left Cliff Shadow -------
-    Sprite* leftCliffShadow = Sprite::create("cliff.png");
-    
-    leftCliffShadow->setColor(Color3B::BLACK);
-    leftCliffShadow->setScaleX(leftCliffWidth / leftCliffShadow->getContentSize().width);
-    leftCliffShadow->setPosition(Point(origin.x + SCALE_640_WIDTH(5, visibleSize.width) + leftCliffWidth * 0.5, visibleSize.height + leftCliffShadow->getContentSize().height - SCALE_960_HEIGHT(15, visibleSize.height)));
-    
-    layer->addChild(leftCliffShadow, 0);
-    
+
     // ------- Right Cliff -------
     double rightCliffWidth = visibleSize.width - (gapWidth + leftCliffWidth); // Whatever is the remaining space
     Sprite* rightCliff = Sprite::create("cliff.png");
     
     rightCliff->setScaleX(rightCliffWidth / rightCliff->getContentSize().width);
+    rightCliff->setColor(Color3B::BLACK);
     rightCliff->setPosition(Point(visibleSize.width - rightCliffWidth * 0.5, visibleSize.height + rightCliff->getContentSize().height));
     
     layer->addChild(rightCliff, 1);
-    
-    // ------- Right Cliff Shadow -------
-    Sprite* rightCliffShadow = Sprite::create("cliff.png");
-    
-    rightCliffShadow->setColor(Color3B::BLACK);
-    rightCliffShadow->setScaleX(rightCliffWidth / rightCliffShadow->getContentSize().width);
-    rightCliffShadow->setPosition(Point(visibleSize.width + SCALE_640_WIDTH(-5, visibleSize.width) - rightCliffWidth * 0.5, visibleSize.height + rightCliffShadow->getContentSize().height - SCALE_960_HEIGHT(15, visibleSize.height)));
-    
-    layer->addChild(rightCliffShadow, 0);
     
     // ------ Moving the cliffs and shadows to the bottom ------
     MoveBy* moveBy = MoveBy::create(10, Vec2(0, -visibleSize.height * 1.2));
 
     leftCliff->runAction(moveBy);
-    leftCliffShadow->runAction(moveBy->clone());
     rightCliff->runAction(moveBy->clone());
-    rightCliffShadow->runAction(moveBy->clone());
 }
