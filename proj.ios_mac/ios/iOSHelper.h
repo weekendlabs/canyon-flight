e42550d42d0750b3e06ee1120ea0a5d6f46445b7
Santhos Baala RS <santhosbaala@systeminsights.com>
2015-03-28 07:55:53 +0530

    :hammer: iOS build working

---
+++ proj.ios_mac/ios/iOSHelper.h
@@ -0,0 +1,21 @@
+//
+//  iOSHelper.h
+//  canyon-flight
+//
+//  Created by Santhos Baala RS on 21/03/15.
+//
+//
+
+#ifndef __canyon_flight__iOSHelper__
+#define __canyon_flight__iOSHelper__
+
+class iOSHelper {
+public:
+    iOSHelper() { }
+    
+    // Admob
+    static void hideAdmobBanner();
+    static void showAdmobBanner();
+};
+
+#endif /* defined(__canyon_flight__iOSHelper__) */
